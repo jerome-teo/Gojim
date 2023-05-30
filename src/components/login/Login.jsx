0a88efdf65c3faa11b4d44db9451a135ddba0d6b
Arthur Zhou <arthuracct@gmail.com>
2023-05-29 22:35:21 -0700

    Login In Progress - Added a submit button that redirects to the profile

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./login.css"
-import Button from 'react-bootstrap/Button';
 
 const Login = () => {
   return (
@@ -9,26 +8,33 @@ const Login = () => {
         Sign In
       </div>
 
-      {/*Sign-in Credentials*/}
-      <div className = "signInCreds">
-        <p>
-        {/* <form action = "/action_page.php"> */}
-        <label for = "username">
-          Username:
-          </label>
-        <input type = "text" id = "username" name = "username" />
-        {/* </form> */}
-        </p>
-        
-        <p>
-        {/* <form action = "/action_page.php"> */}
-        <label for = "password">
-          Password:
-          </label>
-        <input type = "text" id = "password" name = "password" />
-        {/* </form> */}
-        </p>
-      </div>
+      <form action="/profile">
+        {/*Sign-in Credentials*/}
+        <div className = "signInCreds">
+          <p>
+          {/* <form action = "/action_page.php"> */}
+          <label for = "username">
+            Username:
+            </label>
+          <input type = "text" id = "username" name = "username" />
+          {/* </form> */}
+          </p>
+          
+          <p>
+          {/* <form action = "/action_page.php"> */}
+          <label for = "password">
+            Password:
+            </label>
+          <input type = "text" id = "password" name = "password" />
+          {/* </form> */}
+          </p>
+        </div>
+
+        {/*Submit Button*/}
+        <div className = "submitButton">
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
     </div>
   )
 }

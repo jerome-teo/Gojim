5779d5fc73135e24e0d20d833101dda72e40cb3e
Arthur Zhou <arthuracct@gmail.com>
2023-05-29 17:52:32 -0700

    Login Page Completed - Completed login page with username and password text box. Will implement the redirect to different page later, for now I have it commented out

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import "./login.css"
+import Button from 'react-bootstrap/Button';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <div className = "title">
+        Sign In
+      </div>
+
+      {/*Sign-in Credentials*/}
+      <div className = "signInCreds">
+        <p>
+        {/* <form action = "/action_page.php"> */}
+        <label for = "username">
+          Username:
+          </label>
+        <input type = "text" id = "username" name = "username" />
+        {/* </form> */}
+        </p>
+        
+        <p>
+        {/* <form action = "/action_page.php"> */}
+        <label for = "password">
+          Password:
+          </label>
+        <input type = "text" id = "password" name = "password" />
+        {/* </form> */}
+        </p>
+      </div>
+    </div>
   )
 }
 

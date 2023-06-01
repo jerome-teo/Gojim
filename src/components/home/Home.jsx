c351a4dac5622fa46dce0cb240d2f93b17e49880
Brandon Huang <abrandofhuangs@gmail.com>
2023-05-31 19:32:13 -0700

    Finished prototype of home/search page
    Todo: Display search results based on tags (back-end)
          Clear tags upon pressing search button

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,89 @@
 import React from 'react'
+import "./home.css"
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+import { useState } from 'react';
+
+import Button from 'react-bootstrap/Button';
+
+
+const workouts = [
+  {
+    name: "Test Workout 1"
+  },
+  {
+    name: "Test Workout 2"
+  },
+  {
+    name: "Test Workout 3"
+  }
+];
 
 const Home = () => {
+
+  const tags = [
+    {
+      id: 0,
+      name: "Upper-body"
+    },
+    {
+      id: 1,
+      name: "Lower-body"
+    },{
+      id: 2,
+      name: "Beginner"
+    },{
+      id: 3,
+      name: "Intermediate"
+    },{
+      id: 4,
+      name: "Advanced"
+    },
+  ]
+
+  const workoutResults = workouts.map(workoutName =>
+    <li key={workoutName.name} className="list">
+      <Button variant="link">{workoutName.name}</Button>
+    </li>
+  );
+
+  const handleSelect = (tags) => {
+    setTagString(tagString + tags.name + "\n");
+    //Depending on back-end implementation of search, can add tags.name to a data structure here
+  }
+
+  const [tagString, setTagString] = useState("");
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className="homeTitle">
+        Find Your Workout!
+      </div>
+      <div className="tagBox">
+        <p>Tags</p>
+        <textarea readOnly={true} value={tagString}/>
+      </div>
+      <div className='search'>
+        <ReactSearchAutocomplete 
+          styling={{border: "1px solid #000000" }}
+          items={tags}
+          onSelect={handleSelect}
+          placeholder="Enter Tags Here"
+        />
+      </div>
+      <div className="searchButton">
+        <Button variant="secondary">
+          {/*Should update results section*/}
+          Search!
+        </Button>
+      </div>
+      <div className="results">
+        <p>Search Results</p>
+        <ul>
+          {workoutResults}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Home

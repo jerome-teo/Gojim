7143ec191ad8e24c7ba6a526f27c1c2880e106ef
Shivani Hukkeri <hshivani08@gmail.com>
2023-06-06 23:54:16 -0700

    Revamped logic for getter functions
    - getter functions now return json object in certain cases

--- backend/src/listslogic.py
+++ backend/src/listslogic.py
@@ -16,7 +16,6 @@ def create_new_workout():
     add the workout obj to myWorkouts db
     """
 
-
     data = request.json
     workoutName = data.get('workoutName')
     workoutString = data.get('workoutString')
@@ -34,9 +33,10 @@ def create_new_workout():
     
     return jsonify({"status":"workout created"}), 200
 
-@listlogic.route('/get-my-workouts-names', methods=['GET'])
+
+@listlogic.route('/get-my-workouts', methods=['GET'])
 @jwt_required()
-def get_my_workout_names():
+def get_my_workouts():
     """
     Get the current username
     Get all workouts owned by user
@@ -48,48 +48,16 @@ def get_my_workout_names():
     username = data.get('owner')
     username = username[1:len(username)-1]
 
-    workout_names = []
+    workout_json = []
     # workouts owned by user
     workouts = session.query(models.WorkoutLists).filter_by(owner=username).all()
     for w in workouts:
         print("workouts: ")
         print(w)
-        workout_names.append(w.name)
+        workout_json.append({'id': w.id, 'name':w.name, 'info':w.info, 'likes':w.likes})
 
-    # return list of names
-    return workout_names, jsonify({"status":"got my workout names"}), 200
-
-@listlogic.route('/get-workout-info', methods=['GET'])
-@jwt_required()
-def get_workout_info():
-    """
-    Get the current workout
-    return workout.info
-    """
-
-    # get current workout
-
-    # get workout.info
-    # workout_info = workout.info
-
-    # return info
-    # return workout_info, jsonify({"status":"got my workout info"}), 200
-
-@listlogic.route('/get-workout-likes', methods=['GET'])
-@jwt_required()
-def get_workout_likes():
-    """
-    Get the current workout
-    return workout.likes
-    """
-
-    # get current workout
-
-    # get workout.likes
-    # workout_likes = workout.likes
-
-    # return likes
-    # return workout_likes, jsonify({"status":"got my workout likes"}), 200
+    # return json object of workouts
+    return workout_json, jsonify({"status":"got my workouts in json object"}), 200
 
 
 @listlogic.route('/save-workout', methods=['POST'])
@@ -99,21 +67,23 @@ def save_workout():
     get the current user
     add it to savedWorkouts list
     """
+    
+    data = request.json
 
     # get current workout
+    workout_id = data.get("id")
+    workout = session.query(models.User).filter_by(id=workout_id).first()
 
     # get current user
-    data = request.json
     username = data.get('owner') # "name"
     username = username[1:len(username)-1] # name
     user = session.query(models.User).filter_by(username=username).first()
 
     # add saved workout to user
-    # user.saved_workouts.append(workoutObj)
-    # session.commit()
-    
+    user.saved_workouts.append(workout)
+    session.commit()
 
-    return "<p>Saved Workouts</p>"
+    return jsonify({"status":"workout saved!"}), 200
 
 
 @listlogic.route('/get-saved-workout-names', methods=['GET'])
@@ -132,17 +102,17 @@ def get_saved_workout_names():
     user = session.query(models.User).filter_by(username=username).first()
 
     # loop through saved workouts
-    saved_workouts = []
+    saved_workouts_json = []
     for w in user.saved_workouts:
-        saved_workouts.append(w.name)
+        saved_workouts_json.append({'id': w.id, 'name':w.name, 'info':w.info, 'likes':w.likes})
 
-    # RETURNS list of saved workouts' NAMES
-    return saved_workouts, jsonify({"status":"got my workout names"}), 200
+    # return json object of workouts
+    return saved_workouts_json, jsonify({"status":"got saved workouts in json object"}), 200
 
 
 # Home is where this is called
-@listlogic.route('/public-workouts-names', methods=['GET'])
-def public_workouts_names():
+@listlogic.route('/get-public-workouts', methods=['GET'])
+def get_public_workouts():
     """
     loop through users
     if they are public
@@ -150,7 +120,7 @@ def public_workouts_names():
     return set of workouts     
     """
 
-    all_public_workout_names = []
+    all_public_workout_json = []
 
     # get all users
     users = session.query(models.User).all()
@@ -160,17 +130,17 @@ def public_workouts_names():
             user_workouts = session.query(models.WorkoutLists).filter_by(owner=u.username).all()
             # add names of all workouts into list
             for w in user_workouts:
-                all_public_workout_names.append(w.name)
+                all_public_workout_json.append({'id': w.id, 'name':w.name, 'info':w.info, 'likes':w.likes})
 
     # TESTING
-    for work, i in all_public_workout_names:
+    for work, i in all_public_workout_json:
         print("workout " + i + " :")
         print(work)
         i += 1
         print()
 
-    # return list of public workout names
-    return all_public_workout_names
+    # return json object of workouts
+    return all_public_workout_json, jsonify({"status":"got public workouts in json object"}), 200
 
 
 @listlogic.route('/like-workout', methods=['POST'])
@@ -187,19 +157,21 @@ def like_or_unlike_workout():
 
     # get current workout & if it's plus or minus
     data = request.json
+    currWokroutid = data.get("id")
     plus_or_minus = data.get("plus_or_minus")
 
+    workout = session.query(models.WorkoutLists).filter_by(id=currWokroutid).first()
     # get current like count
-    # workout_like = workout.like
+    workout_like = workout.like
 
     # increment or decrement it
     if (plus_or_minus == "plus"):
-        # workout.like = workout_like + 1
-        # session.commit()
+        workout.like = workout_like + 1
+        session.commit()
         return jsonify({"status":"workout liked"}), 200
     elif (plus_or_minus == "minus"):
-        # workout.like = workout_like - 1
-        # session.commit()
+        workout.like = workout_like - 1
+        session.commit()
         return jsonify({"status":"workout unliked"}), 200
 
     return jsonify({"status":"failed to perform action"}), 406
